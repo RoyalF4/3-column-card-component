baa61a5449adfc83b4028e61da0f7a8157c3fe21
Royal Finch <Royal2791@gmail.com>
2024-05-03 17:27:25 -0400

    add styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,10 @@
 function Card({ type, children }) {
   return (
-    <div className="card">
-      <img src="" alt="" className="card__img" />
+    <div className={`card ${type}`}>
+      <img src={`/images/icon-${type}.svg`} alt="" className="card__img" />
       <h2 className="card__heading">{type}</h2>
       <p className="card__description">{children}</p>
-      <a href="" className="card__link">
+      <a href="" className={`card__link ${type}-text`}>
         Learn More
       </a>
     </div>
